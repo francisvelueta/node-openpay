e0596e04fe1f67b88b87bd127c2d7ba64ee287ee
francisvelueta <fvelueta@outlook.com>
2019-01-25 23:36:51 -0600

    upload boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// starting the server
+app.listen(app.get('port'), ()=> {
+  console.log('Server on port', app.get('port'));
+});
