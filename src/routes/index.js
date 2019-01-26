97f429f3ce2c36bd357646b75713868cccd89ec3
francisvelueta <fvelueta@outlook.com>
2019-01-26 01:34:16 -0600

    add layout

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+res.render('signup');
+});
+router.post('/signup', (req, res, next) => {
+console.log(req.body);
+res.send('received');
+});
+
+module.exports = router;
